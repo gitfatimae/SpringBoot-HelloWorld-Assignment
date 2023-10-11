1cd9a4d6079073c29c8519083b9193ebfa419b65
gitfatimae <fatimaezzahraetaibi88@gmail.com>
2023-10-12 00:06:01 +0100

    Helloworld

---
+++ src/test/java/com/example/helloworldsprin/HelloworldsprinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldsprin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldsprinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
